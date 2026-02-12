7139eaa842cb9d15c57f70bc5a14e78bb6dace4f
Peter Gituma <pgituma@myseneca.ca>
2026-02-11 23:05:50 -0500

    First Commit of the Modern Chess App Idea

---
+++ backend/src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("ChessQuest API running");
+});
+
+app.listen(4000, () => {
+  console.log("Server running on port 4000");
+});
